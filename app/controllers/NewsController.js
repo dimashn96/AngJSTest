66dff31b7697e1ea27862e776ec392c9c9d7b756
Dmitriy Shnitkovskiy <dima.shn96@gmail.com>
2018-02-10 16:47:13 +0300

    init local storage service

--- app/controllers/NewsController.js
+++ app/controllers/NewsController.js
@@ -1,26 +1,26 @@
 'use strict';
 
-newsApp.controller('NewsController', function ($rootScope, $scope, dataService, parseService) {
+newsApp.controller('NewsController', function ($rootScope, $scope, dataService, parseService, localStorageService) {
 
-    if (localStorage.sources && localStorage.colors) {
+    if (localStorageService.isset('sources') && localStorageService.isset('colors')) {
         dataService.loadConfig().then(() => {
-            $scope.sources = JSON.parse(localStorage.sources);
-            $scope.colors = JSON.parse(localStorage.colors);
+            $scope.sources = localStorageService.get('sources');
+            $scope.colors = localStorageService.get('colors');
             $scope.updateNews();
         });
     } else {
         dataService.getSources().then((response) => {
             $scope.sources = parseService.parseSources(response.data.sources);
             $scope.colors = parseService.createColors($scope.sources);
-            localStorage.sources = JSON.stringify($scope.sources);
-            localStorage.colors = JSON.stringify($scope.colors);
+            localStorageService.set('sources',$scope.sources);
+            localStorageService.set('colors',$scope.colors);
         });
     }
 
     $scope.updateNews = () => {
         $scope.news = dataService.getNewsBySources($scope.sources,$scope.news);
         $rootScope.news = $scope.news;
-        localStorage.sources = JSON.stringify($scope.sources);
+        localStorageService.set('sources',$scope.sources);
     };
 
     $scope.parseTime = parseService.parseTime;
--- app/services/localStorageService.js
+++ app/services/localStorageService.js
@@ -2,6 +2,16 @@
 
 newsApp.factory('localStorageService', function() {
 
+    let isset = (key) => localStorage[key];
+    let get = (key) => JSON.parse(localStorage[key]);
+    let set = (key,obj) => {
+        localStorage[key] = JSON.stringify(obj);
+    };
 
+    return {
+      isset: isset,
+      get: get,
+      set: set
+    }
 
 });
