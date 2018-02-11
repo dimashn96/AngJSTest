62fd021c6a543579e1670c6f7a27fac74e04954a
Dmitriy Shnitkovskiy <dima.shn96@gmail.com>
2018-02-12 00:59:34 +0300

    edit styles

--- app/controllers/CreateNewsController.js
+++ app/controllers/CreateNewsController.js
@@ -1,10 +1,12 @@
 'use strict';
 
-newsApp.controller('CreateNewsController', function($scope, localStorageService) {
+newsApp.controller('CreateNewsController', function($scope, $location, localStorageService) {
 
     $scope.create = (news,createNewsForm) => {
         if (createNewsForm.$valid) {
             localStorageService.createNews(news.title,news.text,news.author);
+            alert('News created');
+            $location.path('/created');
         }
     }
 
